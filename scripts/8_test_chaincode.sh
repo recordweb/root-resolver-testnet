9d39b35b0445a24a4fb45eb009779908d989d806
Nik <nik@triebwerkstatt.ch>
2026-07-30 15:40:16 +0200

    Namespace for b7d4c810 (BK) adjusted

--- scripts/8_test_chaincode.sh
+++ scripts/8_test_chaincode.sh
@@ -73,7 +73,7 @@ log "=== SEED: Registering four PoC namespaces ==="
 
 declare -A NAMESPACES
 NAMESPACES["a3f9e21c"]="https://vps.recordweb.dev/fragenmanagement/did"
-NAMESPACES["b7d4c810"]="https://resolver.parlament.ch/rwp/v1"
+NAMESPACES["b7d4c810"]="https://vps.recordweb.dev/antwortmanagement/did"
 NAMESPACES["f2c81e05"]="https://resolver.recordweb.org/rwp/v1"
 NAMESPACES["c6cdee0b"]="https://resolver.staatsarchiv.ch/rwp/v1"
 
